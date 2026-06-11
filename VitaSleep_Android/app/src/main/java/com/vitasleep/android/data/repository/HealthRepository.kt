6dbd730d5ca2f517a5b9c32b37a4653f2a42e45c
matchaeryin <matchaer.yin@gmail.com>
2026-06-11 17:53:07 +0800

    fix: remove duplicate ApiResult definition from HealthRepository.kt

--- VitaSleep_Android/app/src/main/java/com/vitasleep/android/data/repository/HealthRepository.kt
+++ VitaSleep_Android/app/src/main/java/com/vitasleep/android/data/repository/HealthRepository.kt
@@ -7,12 +7,6 @@ import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 import javax.inject.Singleton
 
-sealed class ApiResult<out T> {
-    data class Success<T>(val data: T) : ApiResult<T>()
-    data class Error(val message: String, val code: Int? = null) : ApiResult<Nothing>()
-    object Loading : ApiResult<Nothing>()
-}
-
 @Singleton
 class HealthRepository @Inject constructor(
     private val api: VitaSleepApi
