c4300502ad0fc0a48cbf032ccddccfed022f5315
matchaeryin <matchaer.yin@gmail.com>
2026-06-12 14:28:33 +0800

    feat: add Confirming state to ConnectionState

--- VitaSleep_Android/app/src/main/java/com/vitasleep/android/veepoo/ConnectionState.kt
+++ VitaSleep_Android/app/src/main/java/com/vitasleep/android/veepoo/ConnectionState.kt
@@ -1,13 +1,9 @@
 package com.vitasleep.android.veepoo
 
 sealed class ConnectionState {
-    class Disconnected : ConnectionState()
-    class Connecting : ConnectionState()
-    data class Connected(val mac: String) : ConnectionState()
+    data object Disconnected : ConnectionState()
+    data object Connecting : ConnectionState()
+    data object Confirming : ConnectionState()
+    data class Connected(val mac: String, val name: String = "") : ConnectionState()
     data class Error(val message: String) : ConnectionState()
-
-    companion object {
-        val Disconnected = Disconnected()
-        val Connecting = Connecting()
-    }
 }
