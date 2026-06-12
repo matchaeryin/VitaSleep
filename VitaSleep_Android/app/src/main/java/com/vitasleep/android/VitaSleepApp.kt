f3b576922c4c23b4535024879016df8fe9954f3a
matchaeryin <matchaer.yin@gmail.com>
2026-06-12 23:15:18 +0800

    fix: update VitaSleepApp.kt - catch Throwable for crash protection

--- VitaSleep_Android/app/src/main/java/com/vitasleep/android/VitaSleepApp.kt
+++ VitaSleep_Android/app/src/main/java/com/vitasleep/android/VitaSleepApp.kt
@@ -1,7 +1,22 @@
 package com.vitasleep.android
 
 import android.app.Application
+import android.util.Log
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class VitaSleepApp : Application()
+class VitaSleepApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        val defaultHandler = Thread.getDefaultUncaughtExceptionHandler()
+        Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
+            Log.e(
+                "VitaSleepCrash",
+                "Uncaught exception in thread: ${thread.name}",
+                throwable
+            )
+            defaultHandler?.uncaughtException(thread, throwable)
+        }
+    }
+}
