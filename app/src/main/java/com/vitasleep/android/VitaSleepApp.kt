7dae8b9f9a2b766c50f800a81db14cecc26538cd
matchaeryin <matchaer.yin@gmail.com>
2026-06-13 10:38:32 +0800

    fix: add crash log capture + FOREGROUND_SERVICE_LOCATION for v1.0.6

--- app/src/main/java/com/vitasleep/android/VitaSleepApp.kt
+++ app/src/main/java/com/vitasleep/android/VitaSleepApp.kt
@@ -5,6 +5,12 @@ import android.util.Log
 import com.veepoo.protocol.VPOperateManager
 import com.veepoo.protocol.util.VPLogger
 import dagger.hilt.android.HiltAndroidApp
+import java.io.File
+import java.io.PrintWriter
+import java.io.StringWriter
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 @HiltAndroidApp
 class VitaSleepApp : Application() {
@@ -14,11 +20,46 @@ class VitaSleepApp : Application() {
 
         val defaultHandler = Thread.getDefaultUncaughtExceptionHandler()
         Thread.setDefaultUncaughtExceptionHandler { thread, throwable ->
-            Log.e(
-                "VitaSleepCrash",
-                "Uncaught exception in thread: ${thread.name}",
-                throwable
-            )
+            try {
+                val sw = StringWriter()
+                val pw = PrintWriter(sw)
+                val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS", Locale.US)
+                pw.println("=== VitaSleep Crash Log ===")
+                pw.println("Time: ${sdf.format(Date())}")
+                pw.println("Thread: ${thread.name}")
+                pw.println("Exception: ${throwable.javaClass.name}")
+                pw.println("Message: ${throwable.message}")
+                pw.println()
+                pw.println("--- Stack Trace ---")
+                throwable.printStackTrace(pw)
+                pw.println()
+                pw.println("--- Cause ---")
+                val cause = throwable.cause
+                if (cause != null) {
+                    pw.println("Cause: ${cause.javaClass.name}: ${cause.message}")
+                    cause.printStackTrace(pw)
+                } else {
+                    pw.println("No cause")
+                }
+                pw.println()
+                pw.println("--- Suppressed ---")
+                for (suppressed in throwable.suppressed) {
+                    pw.println("Suppressed: ${suppressed.javaClass.name}: ${suppressed.message}")
+                    suppressed.printStackTrace(pw)
+                }
+                pw.flush()
+
+                val crashLog = sw.toString()
+                Log.e("VitaSleepCrash", crashLog)
+
+                val file = File(filesDir, "last_crash.log")
+                file.writeText(crashLog)
+
+                val historyFile = File(filesDir, "crash_history.log")
+                val historyEntry = "\n\n${"=".repeat(60)}\n${crashLog}"
+                historyFile.appendText(historyEntry)
+            } catch (_: Throwable) {
+            }
             defaultHandler?.uncaughtException(thread, throwable)
         }
 
@@ -35,6 +76,22 @@ class VitaSleepApp : Application() {
             com.jieli.jl_rcsp.util.JL_Log.setLog(true)
         } catch (e: Throwable) {
             Log.e("VitaSleepApp", "JL_Log init failed", e)
+        }
+    }
+
+    fun getLastCrashLog(): String? {
+        return try {
+            val file = File(filesDir, "last_crash.log")
+            if (file.exists()) file.readText() else null
+        } catch (_: Throwable) {
+            null
+        }
+    }
+
+    fun clearCrashLog() {
+        try {
+            File(filesDir, "last_crash.log").delete()
+        } catch (_: Throwable) {
         }
     }
 }
